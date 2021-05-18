8ada582b487be3e37315e44a2d27a18d8d9f605f
karenanndonnachie <karenanndonnachie@gmail.com>
2021-05-18 15:50:07 +1000

    Update generativeglyph_test_wsvg.js

--- debugging/generativeglyph_test_wsvg/generativeglyph_test_wsvg.js
+++ debugging/generativeglyph_test_wsvg/generativeglyph_test_wsvg.js
@@ -2,14 +2,14 @@ var t;
 var myText = 'click to pause/play, press r to reset canvas, hit enter to save!';
 
 function setup() {
-  createCanvas(800, 800, SVG);
+  createCanvas(800, 600, SVG);
   //stroke(10, 100);
   strokeWeight(2);
   stroke('#ED225D');
   noFill();
   t = 0;
   background(120);
-  frameRate(20);
+  frameRate(10);
 }
 
 function draw() {
@@ -17,6 +17,7 @@ function draw() {
   var x = frameCount / 100;
   var y = sin(x * PI * 2);
   line(x * width, height * 0.5, x * width, y * height / 2 + height * 0.5);
+  ellipse(width/2, height/2, 100, 100);
   }
   
 
