f2c11fc8c93aaf186c790c162d5fd39fd59344b5
karenanndonnachie <karenanndonnachie@gmail.com>
2021-05-30 21:33:43 +1000

    variable font mapped to mouseX

--- debugging/sylvain_test/sylvain_test.js
+++ debugging/sylvain_test/sylvain_test.js
@@ -1,3 +1,8 @@
+ /* 
+  * A to the K to the Z 2021 Studio 
+  * Sketch developed by Karen ann Donnachie to display & manipulate variable types in P5js Canvas 
+  * 
+  */
 var pfat=100;
 function setup() {
   cnv=createCanvas(windowWidth, windowHeight);
@@ -10,11 +15,10 @@ function draw() {
   fill(220);
   pfat=map(mouseX, 0, width, 100, 900);
   checkLength();
-  textFont('vartext', 100);
+  //textFont('vartext', 100);
   text('Hello', width/2, height/2);
 }
 function checkLength(){
-
   fill(220);
   let fs="'wght' "+ pfat;
   cnv.style('font-variation-settings', fs);
