900b292ed4b0def8317a08e334725f7ddde9c071
karenanndonnachie <karenanndonnachie@gmail.com>
2021-05-30 21:02:36 +1000

    variable font mapped to mouseX

---
+++ debugging/sylvain_test/sylvain_test.js
@@ -0,0 +1,25 @@
+var pfat=100;
+function setup() {
+  cnv=createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER,BOTTOM);
+  textFont('vartext', pfat);
+}
+
+function draw() {
+  background(60);
+  fill(220);
+  pfat=map(mouseX, 0, width, 100, 900);
+  checkLength();
+  textFont('vartext', 100);
+  text('Hello', width/2, height/2);
+}
+function checkLength(){
+
+  fill(220);
+  let fs="'wght' "+ pfat;
+  cnv.style('font-variation-settings', fs);
+  console.log(fs);
+  //fontWeight(pfat);
+  textFont('vartext', 100);
+  text('Hello', width/2, height/2);
+}
